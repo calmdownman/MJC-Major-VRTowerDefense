8b3cc75e5c3a83e7bfb5bb91fc269006e5226cde
calmdownman <cskagud7@naver.com>
2023-06-09 22:19:30 +0900

    Add Bomb Explosion Fn

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    //폭발 효과
+    Transform explosion;
+    ParticleSystem expEffect;
+    AudioSource expAudio;
+    //폭발 영역
+    public float range = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //씬에서 Explision 객체를 찾아 transform 정보 가져오기
+        explosion = GameObject.Find("Explosion").transform;
+        expEffect = explosion.GetComponent<ParticleSystem>();
+        expAudio = explosion.GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int layerMask = 1 << LayerMask.NameToLayer("Drone");
+        Collider[] drones = Physics.OverlapSphere(transform.position, range, layerMask);
+        foreach (Collider drone in drones)
+        {
+            Destroy(drone.gameObject);
+        }
+        explosion.position = transform.position; //폭발 효과의 위치 지정
+        expEffect.Play(); //이첵트 재생
+        expAudio.Play(); //이펙트 사운드 재생
+        Destroy(gameObject); //폭탄 없애기
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DroneManger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneManger : MonoBehaviour
+{
+    //랜덤 시간의 범위
+    public float minTime = 1;
+    public float maxTime = 5;
+    float createTime; //생성 시간
+    float currentTime; //경과 시간
+    public Transform[] spawnPoints; //드론 생성 위치
+    public GameObject droneFactory; //드론 공장
+    // Start is called before the first frame update
+    void Start()
+    {
+        createTime = Random.Range(minTime, maxTime); //생성 시간을 랜덤 범위에서 설정
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > createTime) //경과 시간이 생성시간을 초과하였다면...
+        { 
+            GameObject drone = Instantiate(droneFactory); //드론 생성
+            int index = Random.Range(0, spawnPoints.Length); //드론 위치 랜덤 설정
+            drone.transform.position = spawnPoints[index].position;
+            currentTime = 0; //경과 시간 초기화
+            createTime = Random.Range(minTime, maxTime); //생성 시간 재할당
+        }
+    }
+}
